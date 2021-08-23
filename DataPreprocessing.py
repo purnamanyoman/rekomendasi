9526589ddc8f3a906fba1c75ec938dce965fe851
purnamanyoman <88636041+purnamanyoman@users.noreply.github.com>
2021-08-23 15:22:13 +0800

    Add files via upload

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -16,7 +16,7 @@ def Data_Preprocessing(data_set):
 
  
 def RemoveNullValues(data_set):
-    hav_null=['formatted_price','color','brand']
+    hav_null=['nama_villa','jumlah_bedroom']
     for features in hav_null:
         data_set=data_set.loc[-data_set[features].isnull()]
         print("\n\nNo. of data points after eliminating {}=NULL :\n".format(features),data_set.shape[0])
@@ -25,9 +25,9 @@ def RemoveNullValues(data_set):
 #Near Duplicate items remain
 #remove data differing only at end(that is size difference or colour difference are not good recommendations)
 def RemoveDuplicateItemsAfterSorting(data_set):
-    print("\nThe duplicated items are : ",sum(data_set.duplicated('title')),"\n\n")
+    print("\nThe duplicated items are : ",sum(data_set.duplicated('nama_villa')),"\n\n")
     #sort on the basis of the title(alphabetical order)
-    data_set.sort_values('title',inplace=True,ascending=False)
+    data_set.sort_values('nama_villa',inplace=True,ascending=False)
     #print(data_set['title'].head(20))
     
     row_wise_str_index=[]
@@ -40,10 +40,10 @@ def RemoveDuplicateItemsAfterSorting(data_set):
     while i<len_data_points and j<len_data_points:
         temp_i=i
         #wordlist for ith string
-        i_str = data_set['title'].loc[row_wise_str_index[i]].split()
+        i_str = data_set['nama_villa'].loc[row_wise_str_index[i]].split()
         j=i+1
         while j<len_data_points:
-            j_str = data_set['title'].loc[row_wise_str_index[j]].split()
+            j_str = data_set['nama_villa'].loc[row_wise_str_index[j]].split()
             max_len=max(len(i_str),len(j_str))
             count=0 #no. of words matching in both strings
             #itertools.zip_longest(i_str,j_str) return correspoinding words in format [('a1','b1'),('a2','b2'),(None,'b3')]
@@ -52,17 +52,17 @@ def RemoveDuplicateItemsAfterSorting(data_set):
                     count+=1
             if(max_len-count>2):
                 #word difference>2 both different items -- hence include
-                new_data_list.append(data_set['asin'].loc[row_wise_str_index[j]])
+                new_data_list.append(data_set['web-scraper-order'].loc[row_wise_str_index[j]])
                 if(max_len-1==j):
                     #word difference>2 but in len_data_points and len_data_points-1
-                    new_data_list.append(data_set['asin'].loc[row_wise_str_index[j]])
+                    new_data_list.append(data_set['web-scraper-order'].loc[row_wise_str_index[j]])
                 i=j
                 break
             else:
                 j+=1
         if temp_i == i:
             break   
-    data_set=data_set.loc[data_set['asin'].isin(new_data_list)]
+    data_set=data_set.loc[data_set['web-scraper-order'].isin(new_data_list)]
     return data_set
 
 def RemoveDuplicateItemsNonAdjacent(data_set): # Time complexity = O(n^2) 
@@ -73,10 +73,10 @@ def RemoveDuplicateItemsNonAdjacent(data_set): # Time complexity = O(n^2)
     while(len(row_wise_str_index)!=0):
         #remove the last element in the row_wise_str_index and return the index of the last element
         i = row_wise_str_index.pop()
-        new_data_list.append(data_set['asin'].loc[i])
-        i_str=data_set['title'].loc[i].split()
+        new_data_list.append(data_set['web-scraper-order'].loc[i])
+        i_str=data_set['nama_villa'].loc[i].split()
         for j in row_wise_str_index:
-            j_str = data_set['title'].loc[j].split()
+            j_str = data_set['nama_villa'].loc[j].split()
             max_len = max(len(i_str),len(j_str))
             count = 0 #count of mathing words in the two strings
             for words in itertools.zip_longest(i_str,j_str):
